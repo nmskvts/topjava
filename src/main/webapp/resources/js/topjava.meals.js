38f2cd09291e5919cd1ca0e268672c300b9842cb
Anastasia <www.anastasiamoskivets@gmail.com>
2020-04-11 15:43:28 +0300

    Add MealAjaxController

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,33 @@
+$(function () {
+    makeEditable({
+        ajaxURL: "ajax/profile/meals",
+        datatableApi: $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    "desc"
+                ]
+            ]
+        })
+    })
+});
